dd4b58faeae62bf1b437458c2d9cf9433639f0b2
XimicYT <abutts29@students.unorth.k12.in.us>
2026-08-18 12:16:12 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { 
+    cors: { 
+        origin: "*",
+        methods: ["GET", "POST"]
+    } 
+});
+
+// Render serves your static frontend files automatically
+app.use(express.static(__dirname));
+const players = {};
+
+io.on('connection', (socket) => {
+    players[socket.id] = {
+        x: Math.floor(Math.random() * 750) + 25,
+        y: Math.floor(Math.random() * 550) + 25,
+        color: `hsl(${Math.random() * 360}, 70%, 50%)`,
+        id: socket.id
+    };
+
+    socket.emit('currentPlayers', players);
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('playerMovement', (movementData) => {
+        if (players[socket.id]) {
+            players[socket.id].x = movementData.x;
+            players[socket.id].y = movementData.y;
+            socket.broadcast.emit('playerMoved', players[socket.id]);
+        }
+    });
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', { id: socket.id, message: msg });
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('playerDisconnected', socket.id);
+    });
+});
+
+// Render injects the PORT environment variable automatically
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
